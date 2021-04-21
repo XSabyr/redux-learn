68ec12830085e659655c8f9455932212df3bed30
Khotak Sabur <sabur.xotak@mail.ru>
2021-04-21 18:00:20 +0300

    minor error fixed

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import loginReducer from './reducers/loginSlice';
 import userSlice from './reducers/userSlice';
 
 export default configureStore({
